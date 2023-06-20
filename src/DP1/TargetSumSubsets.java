3818f6375acccad3ee4223029df3891791b57cce
krishnendu <krishnenduduttadc@gmail.com>
2023-06-20 23:47:15 +0530

    sixth commit

--- src/DP1/TargetSumSubsets.java
+++ src/DP1/TargetSumSubsets.java
@@ -6,9 +6,37 @@
 package DP1;
 
 /**
- *
  * @author krish
  */
 public class TargetSumSubsets {
-    
+    public static void main(String[] args) {
+        int arr[] = {4,2,7,1,3};
+        int n = arr.length;
+        int tar = 10;
+        boolean[][] dp = new boolean[n + 1][tar + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } else if (i == 0) {
+                    dp[i][j] = false;
+                } else if (j == 0) {
+                    dp[i][j] = true;
+                } else {
+                    if (dp[i - 1][j] == true) {
+                        dp[i][j] = true;
+                    } else {
+                        int val = arr[i - 1];
+                        if (j >= val) {
+                            if (dp[i - 1][j - val] == true) {
+                                dp[i][j] = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(dp[arr.length][tar]);
+    }
 }
