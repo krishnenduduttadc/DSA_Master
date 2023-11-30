10469024492d2cd46789d9a6fe17cf78557ef856
krishnendu <krishnenduduttadc@gmail.com>
2023-11-30 21:29:34 +0530

    added

--- src/RecursionAndBacktrackingL2/CoinChangeCombination1.java
+++ src/RecursionAndBacktrackingL2/CoinChangeCombination1.java
@@ -1,8 +1,5 @@
 package RecursionAndBacktrackingL2;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class CoinChangeCombination1 {
     public static void coinChange(int i, int[] coins, int amtsf, int tamt, String asf){
         // write your code here
@@ -16,14 +13,8 @@ public class CoinChangeCombination1 {
         coinChange(i+1,coins,amtsf+0,tamt,asf);
     }
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] coins = new int[n];
-        for (int i = 0; i < n; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
-        }
-        int amt = Integer.parseInt(br.readLine());
-
+        int[] coins = {2,3,5,6,7};
+        int amt = 12;
         coinChange(0, coins, 0, amt, "");
     }
 }
--- src/RecursionAndBacktrackingL2/CoinChangeCombination2.java
+++ src/RecursionAndBacktrackingL2/CoinChangeCombination2.java
@@ -1,8 +1,5 @@
 package RecursionAndBacktrackingL2;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class CoinChangeCombination2 {
     public static void coinChange(int i, int[] coins, int amtsf, int tamt, String asf) {
         // write your code here
@@ -26,13 +23,8 @@ public class CoinChangeCombination2 {
     }
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] coins = new int[n];
-        for (int i = 0; i < n; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
-        }
-        int amt = Integer.parseInt(br.readLine());
+        int[] coins = {2,3,5,6,7};
+        int amt = 12;
         coinChange(0, coins, 0, amt, "");
     }
 }
