22e34b1d382a3b61654a4a74e2efe23ffc1e7f92
krishnendu <krishnenduduttadc@gmail.com>
2024-02-05 21:04:42 +0530

    added

--- src/Striver_graph/EventualSafeState.java
+++ src/Striver_graph/EventualSafeState.java
@@ -4,47 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EventualSafeState {
-    private boolean dfsCheck(int node, List<List<Integer>> adj, int vis[],
-                             int pathVis[], int check[]) {
-        vis[node] = 1;
-        pathVis[node] = 1;
-        check[node] = 0;
-        // traverse for adjacent nodes
-        for (int it : adj.get(node)) {
-            // when the node is not visited
-            if (vis[it] == 0) {
-                if (dfsCheck(it, adj, vis, pathVis, check) == true)
-                    return true;
-            }
-            // if the node has been previously visited
-            // but it has to be visited on the same path
-            else if (pathVis[it] == 1) {
-                return true;
-            }
-        }
-        check[node] = 1;
-        pathVis[node] = 0;
-        return false;
-    }
-
-    List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj) {
-        int vis[] = new int[V];
-        int pathVis[] = new int[V];
-        int check[] = new int[V];
-        for (int i = 0; i < V; i++) {
-            if (vis[i] == 0) {
-                dfsCheck(i, adj, vis, pathVis, check);
-            }
-        }
-        List<Integer> safeNodes = new ArrayList<>();
-        for (int i = 0; i < V; i++) {
-            if (check[i] == 1)
-                safeNodes.add(i);
-        }
-        return safeNodes;
-        // Your code here
-    }
-
     public static void main(String[] args) {
         int V = 12;
         List<List<Integer>> adj = new ArrayList<>();
@@ -71,5 +30,43 @@ public class EventualSafeState {
             System.out.print(node + " ");
         }
         System.out.println("");
+    }
+
+    List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj) {
+        int vis[] = new int[V];
+        int pathVis[] = new int[V];
+        int check[] = new int[V];
+        for (int i = 0; i < V; i++) {
+            if (vis[i] == 0) {
+                dfsCheck(i, adj, vis, pathVis, check);
+            }
+        }
+        List<Integer> safeNodes = new ArrayList<>();
+        for (int i = 0; i <V; i++) {
+            if (check[i] == 01) {
+                safeNodes.add(i);
+            }
+        }
+        return safeNodes;
+    }
+
+    private boolean dfsCheck(int s, List<List<Integer>> adj, int vis[],
+                             int pathVis[], int check[]) {
+
+        vis[s] = 1;
+        pathVis[s] = 1;
+        check[s] = 0;
+        for (int it : adj.get(s)) {
+            if (vis[it] == 0) {
+                if (dfsCheck(it, adj, vis, pathVis, check) == true) {
+                    return true;
+                }
+            } else if (pathVis[it] == 1) {
+                return true;
+            }
+        }
+        check[s]=1;
+        pathVis[s]=0;
+        return false;
     }
 }
--- src/Striver_graph/ShortestPathinUndirectedGraphUnitWeights.java
+++ src/Striver_graph/ShortestPathinUndirectedGraphUnitWeights.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class ShortestPathinUndirectedGraphUnitWeights {
+
     public static void main(String[] args) throws IOException {
         int n=9, m=10;
         int[][] edge = {{0,1},{0,3},{3,4},{4,5},{5,6},{1,2},{2,6},{6,7},{7,8},{6,8}};
@@ -18,37 +19,32 @@ public class ShortestPathinUndirectedGraphUnitWeights {
         System.out.println();
     }
 
-    public int[] shortestPath(int[][] edges,int n,int m ,int src) {
-        //Create an adjacency list of size N for storing the undirected graph.
+    public int[] shortestPath(int[][] edges,int n,int m ,int s) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for(int i = 0;i<n;i++) {
             adj.add(new ArrayList<>());
         }
+
         for(int i = 0;i<m;i++) {
             adj.get(edges[i][0]).add(edges[i][1]);
             adj.get(edges[i][1]).add(edges[i][0]);
         }
-        //A dist array of size N initialised with a large number to
-        //indicate that initially all the nodes are untraversed.
         int dist[] = new int[n];
         for(int i = 0;i<n;i++) dist[i] = (int)1e9;
-        dist[src] = 0;
-
-        // BFS Implementation
+        dist[s]=0;
         Queue<Integer> q = new LinkedList<>();
-        q.add(src);
+        q.add(s);
         while(!q.isEmpty()) {
             int node = q.peek();
             q.remove();
-            for(int it : adj.get(node)) {
-                if(dist[node] + 1 < dist[it]) {
-                    dist[it] = 1 + dist[node];
+            for(int it:adj.get(node)){
+                if(dist[node]+1<dist[it]){
+                    dist[it]=1+dist[node];
                     q.add(it);
                 }
             }
         }
-        // Updated shortest distances are stored in the resultant array ‘ans’.
-        // Unreachable nodes are marked as -1.
+
         for(int i = 0;i<n;i++) {
             if(dist[i] == 1e9) {
                 dist[i] = -1;
