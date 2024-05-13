ec4626e897f8bff5ca87cb5bac1402c02b9e7a07
krishnendu <krishnenduduttadc@gmail.com>
2024-05-13 22:46:04 +0530

    added

---
+++ src/BitManipulationResearch/MyShift.java
@@ -0,0 +1,12 @@
+package BitManipulationResearch;
+
+public class MyShift {
+    public static void main(String[] args) {
+        int x1=14;
+        int res1=x1<<2;
+        System.out.println(res1);
+
+        int res2=x1>>2;
+        System.out.println(res2);
+    }
+}
