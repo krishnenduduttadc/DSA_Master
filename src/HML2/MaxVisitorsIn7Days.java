3c5523ed82e0cb3ef8fa963b6dbb94588bf9886f
krishnendu <krishnenduduttadc@gmail.com>
2025-07-09 23:42:09 +0530

    added

--- src/HML2/MaxVisitorsIn7Days.java
+++ src/HML2/MaxVisitorsIn7Days.java
@@ -1,7 +1,5 @@
 package HML2;
 
-import java.util.HashMap;
-
 public class MaxVisitorsIn7Days {
     public static void main(String[] args) {
         int[] visitors = {10, 15, 20, 18, 25, 25, 25, 25, 25, 19, 23, 28, 31};
@@ -12,28 +10,22 @@ public class MaxVisitorsIn7Days {
     }
 
     public static int findMaxVisitors(int[] arr, int k) {
-        int maxVisitors = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i <= k - 2; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-        }
-        for (int i = k - 1, j = 0; i < arr.length; i++, j++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-            int max = Integer.MIN_VALUE;
-            for (int m : map.keySet()) {
-                if (map.get(m) > max) {
-                    max = map.get(m);
-                }
-            }
-            maxVisitors=Math.max(maxVisitors,max);
+        int sum = 0;
+        int maxSum = 0;
 
-            // Release old element
-            if (map.get(arr[j]) == 1) {
-                map.remove(arr[j]);
-            } else {
-                map.put(arr[j], map.get(arr[j]) - 1);
-            }
+        // Sum of first k elements
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
         }
-        return maxVisitors;
+
+        maxSum = sum;
+
+        // Sliding window
+        for (int i = k; i < arr.length; i++) {
+            sum = sum - arr[i - k] + arr[i];
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum;
     }
 }
--- src/HML2/SmallestSubarrayWithHighestFrequencyElement.java
+++ src/HML2/SmallestSubarrayWithHighestFrequencyElement.java
@@ -4,44 +4,46 @@ import java.util.HashMap;
 
 public class SmallestSubarrayWithHighestFrequencyElement {
     public static void main(String[] args) {
-        int arr[]={1,3,2,4,2,3,4,2,5,6,5,5,7};
+        int arr[] = {1, 3, 2, 4, 2, 3, 4, 2, 5, 6, 5, 5, 7};
         solution(arr);
     }
 
-    static void solution(int[] arr){
-        HashMap<Integer,Integer> fmap=new HashMap<>();
-        HashMap<Integer,Integer> smap=new HashMap<>();
-        int si=0;
-        int ei=0;
-        int len=ei-si+1;
-        int hfreq=0;
-        for (int i = 0; i <arr.length ; i++) {
-            if(fmap.containsKey(arr[i])){
-                fmap.put(arr[i],fmap.get(arr[i])+1);
-            }else{
-                fmap.put(arr[i],1);
-                smap.put(arr[i],1);
+    static void solution(int[] arr) {
+        HashMap<Integer, Integer> fmap = new HashMap<>();
+        HashMap<Integer, Integer> smap = new HashMap<>();
+        int si = 0;
+        int ei = 0;
+        int len = ei - si + 1;
+        int hfreq = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int val = arr[i];
+            fmap.put(val, fmap.getOrDefault(val, 0) + 1);
+            if (!smap.containsKey(val)) {
+                smap.put(val, i); // store first occurrence
             }
 
-            if(fmap.get(arr[i])>hfreq){
-                hfreq=fmap.get(arr[i]);
-                si=smap.get(arr[i]);
-                ei=i;
-                len=ei-si+1;
-
-            }else if(fmap.get(arr[i])==hfreq){
-                int clen=i-smap.get(arr[i])+1;
-                if(clen<len){
-                    hfreq=fmap.get(arr[i]);
-                    si=smap.get(arr[i]);
-                    ei=i;
-                    len=ei-si+1;
+            if (fmap.get(val) > hfreq) {
+                hfreq = fmap.get(val);
+                si = smap.get(val);
+                ei = i;
+                len = ei - si + 1;
+            } else if (fmap.get(val) == hfreq) {
+                int clen = i - smap.get(val) + 1;
+                if (clen < len) {
+                    si = smap.get(val);
+                    ei = i;
+                    len = ei - si + 1;
                 }
             }
         }
-        System.out.println(arr[si]);
-        System.out.println(si);
-        System.out.println(ei);
 
+        System.out.println("Highest frequency element: " + arr[si]);
+        System.out.println("Start index: " + si);
+        System.out.println("End index: " + ei);
+        System.out.print("Subarray: ");
+        for (int i = si; i <= ei; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
