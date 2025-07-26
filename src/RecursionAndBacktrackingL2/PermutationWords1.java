edc13721ae1960474021ce25f16293a8fc6d2108
krishnendu <krishnenduduttadc@gmail.com>
2025-07-27 00:45:04 +0530

    added

--- src/RecursionAndBacktrackingL2/PermutationWords1.java
+++ src/RecursionAndBacktrackingL2/PermutationWords1.java
@@ -1,34 +1,32 @@
 package RecursionAndBacktrackingL2;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class PermutationWords1 {
     public static void generateWords(int cs, int ts, HashMap<Character, Integer> fmap, String asf) {
         // write your code here
 
-        if(cs>ts){
+        if (cs > ts) {
             System.out.println(asf);
             return;
 
         }
-        for(char ch:fmap.keySet()){
-            if(fmap.get(ch)>0){
-                fmap.put(ch,fmap.get(ch)-1);
-                generateWords(cs+1,ts,fmap,asf+ch);
-                fmap.put(ch,fmap.get(ch)+1);
+        for (char ch : fmap.keySet()) {
+            if (fmap.get(ch) > 0) {
+                fmap.put(ch, fmap.get(ch) - 1);
+                generateWords(cs + 1, ts, fmap, asf + ch);
+                fmap.put(ch, fmap.get(ch) + 1);
             }
         }
     }
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = "aabb";
 
         HashMap<Character, Integer> fmap = new HashMap<>();
-        for(char ch: str.toCharArray()){
-            if(fmap.containsKey(ch)){
+        for (char ch : str.toCharArray()) {
+            if (fmap.containsKey(ch)) {
                 fmap.put(ch, fmap.get(ch) + 1);
             } else {
                 fmap.put(ch, 1);
@@ -39,6 +37,4 @@ public class PermutationWords1 {
     }
 }
 
-/*
-aabb
- */
+
--- src/StringsL1/ToggleOpticaDocument.java
+++ src/StringsL1/ToggleOpticaDocument.java
@@ -6,17 +6,33 @@ public class ToggleOpticaDocument {
         System.out.println(toggleCase(str));
     }
 
-    static String toggleCase(String str){
-        StringBuilder sb=new StringBuilder(str);
+    static String toggleCase(String str) {
+        StringBuilder sb = new StringBuilder(str);
         for (int i = 0; i < sb.length(); i++) {
-            char ch=sb.charAt(i);
-            if(ch>='a' && ch<='z'){
-                char uch=(char)('A'+ch-'a');
-                sb.setCharAt(i,uch);
-            }else if(ch>='A' && ch<='Z'){
-                char lch=(char)('a'+ch-'A');
-                sb.setCharAt(i,lch);
+            char ch = sb.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                char uch = (char) ('A' + ch - 'a');
+                sb.setCharAt(i, uch);
+            } else if (ch >= 'A' && ch <= 'Z') {
+                char lch = (char) ('a' + ch - 'A');
+                sb.setCharAt(i, lch);
             }
+        }
+        return sb.toString();
+
+    }
+
+
+    static String toggleCase2(String str) {
+        char[] chr = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chr.length; i++) {
+            if (chr[i] >= 65 && chr[i] <= 90) {
+                chr[i] = (char) (chr[i] + 32);
+            } else {
+                chr[i] = (char) (chr[i] - 32);
+            }
+            sb.insert(i, chr[i]);
         }
         return sb.toString();
 
