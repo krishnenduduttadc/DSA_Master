9be235517be5b036f86fe08f1591a48b19adafd2
krishnendu <krishnenduduttadc@gmail.com>
2025-09-19 00:06:46 +0530

    added

--- src/Concepts/BackEdge.java
+++ src/Concepts/BackEdge.java
@@ -3,70 +3,59 @@ package Concepts;
 import java.util.ArrayList;
 
 public class BackEdge {
-    static int V = 7;
+    static int V = 7;                      // vertices are 0..6
     static ArrayList<Integer>[] al = new ArrayList[V];
-    static boolean[] visited = new boolean[V+1];
+    static boolean[] visited = new boolean[V];
 
     public static void main(String[] args) {
 
-
-        // initializing
-        for (int i = 0; i < al.length; i++) {
-            al[i] = new ArrayList<Integer>();
+        // initialize adjacency lists for 0..V-1
+        for (int i = 0; i < V; i++) {
+            al[i] = new ArrayList<>();
         }
 
-        // We can add any number of columns to each
-        // rows as per our wish
+        // --- Example edges (0-based) ---
+        // adjust these to match the exact graph you want
         al[0].add(1);
         al[0].add(2);
         al[0].add(3);
 
         al[1].add(2);
-        al[1].add(1);
         al[1].add(3);
 
         al[2].add(3);
         al[2].add(1);
-        al[2].add(2);
 
         al[3].add(4);
         al[3].add(2);
         al[3].add(5);
 
         al[4].add(5);
-        al[4].add(4);
         al[4].add(6);
 
         al[5].add(6);
-        al[5].add(5);
-        al[5].add(7);
+        al[5].add(4);
 
-        al[6].add(7);
         al[6].add(4);
-        al[6].add(6);
+        al[6].add(5);
 
-//        for (int i = 0; i < V; i++) {
-//            for (int j = 0; j < al[i].size(); j++) {
-//                System.out.print(al[i].get(j) + " ");
-//            }
-//            System.out.println();
-//        }
-
-        for (int i = 1; i <= V ; i++) {
-            if(visited[i]==false){
-                DFSR( i, 0);
+        // DFS for all components (0..V-1)
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                DFSR(i, -1);   // -1 means "no parent" (root)
             }
         }
     }
 
-    private static void DFSR( int cur, int par) {
-        System.out.println(cur + " ");
+    private static void DFSR(int cur, int par) {
+        System.out.println("visit: " + cur);
         visited[cur] = true;
         for (int nbr : al[cur]) {
-            if (visited[nbr] == false) {
-                DFSR( nbr, cur);
+            if (!visited[nbr]) {
+                DFSR(nbr, cur);
             } else if (nbr != par) {
-                System.out.println(cur + "  " + nbr);
+                // found a back-edge in an undirected-style graph
+                System.out.println("back-edge: " + cur + " -> " + nbr);
             }
         }
     }
