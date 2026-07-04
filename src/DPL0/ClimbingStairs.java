3513ad8be3bd4d2095ba8b33fdc3f5ce101f47b9
krishnendu <krishnenduduttadc@gmail.com>
2026-07-05 00:03:51 +0530

    added

--- src/DPL0/ClimbingStairs.java
+++ src/DPL0/ClimbingStairs.java
@@ -4,14 +4,19 @@ public class ClimbingStairs {
     public static void main(String[] args) {
         int arr[] = {1, 5, 2, 3, 1};
         int n = arr.length;
+        climb(arr, n);
+
+    }
+
+    static void climb(int[] arr, int n) {
         Integer dp[] = new Integer[n + 1];
         dp[n] = 0;
         for (int i = n - 1; i >= 0; i--) {
-            if(arr[i]>0){
-                int min=Integer.MAX_VALUE;
-                for (int j = 1; j <=arr[i] && (i+j)<dp.length; j++) {
-                    if(dp[i+j]!=null){
-                        min=Math.min(min,dp[i+j]);
+            if (arr[i] > 0) {
+                int min = Integer.MAX_VALUE;
+                for (int j = 1; j <= arr[i] && (i + j) < dp.length; j++) {
+                    if (dp[i + j] != null) {
+                        min = Math.min(min, dp[i + j]);
                     }
                 }
                 if (min != Integer.MAX_VALUE) {
